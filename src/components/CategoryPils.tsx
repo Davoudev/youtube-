2e5a0d42a39c373cea56998eafaeda42d6bb1d35
david <davidchaharmehali@gmail.com>
2024-12-24 00:13:40 +0330

    add category pills section

--- src/components/CategoryPils.tsx
+++ src/components/CategoryPils.tsx
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Button from "./Button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
-const CategoryPils = () => {
-  return <div>CategoryPils</div>;
+type CategoryPillsProps = {
+  categories: string[];
+  selectedCategory: string;
+  onselect: (category: string) => void;
+};
+const CategoryPils = ({
+  categories,
+  selectedCategory,
+  onselect,
+}: CategoryPillsProps) => {
+  const [translate, setTranslate] = useState(0);
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current == null) return;
+
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0]?.target;
+      if (container == null) return;
+
+      setIsLeftVisible(translate > 0);
+      setIsRightVisible(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [translate, categories]);
+
+  const TRANSLATE_AMOUNT = 200;
+
+  return (
+    <div className="relative overflow-x-hidden" ref={containerRef}>
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
+        {categories.map((category) => (
+          <Button
+            variant={selectedCategory === category ? "default" : "ghost"}
+            className="py-1 px-3 rounded-lg whitespace-nowrap"
+            onClick={() => onselect(category)}
+            key={category}
+          >
+            {category}
+          </Button>
+        ))}
+      </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              });
+            }}
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current == null) {
+                  return translate;
+                }
+                const newTranslate = translate + TRANSLATE_AMOUNT;
+                const edge = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+
+                if (newTranslate + width >= edge) {
+                  return edge - width;
+                }
+
+                return newTranslate;
+              });
+            }}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CategoryPils;
